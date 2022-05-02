aa4db6cc237a6e89450318daada55f65d368239a
MSufra <matthewsufra@gmail.com>
2022-05-02 22:33:18 +1000

    app,index,patient update

--- app.py
+++ app.py
@@ -5,9 +5,30 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import load_model
 import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, Column, Integer, String
+
 
 app = Flask(__name__)
 
+#DATABASE SETUP#
+Base = declarative_base()
+
+class Patient(Base):
+    __tablename__ = 'patient'
+    id = Column(Integer, primary_key=True)
+    fname = Column(String(50))
+    lname = Column(String(50))
+    age = Column(Integer)
+    sex = Column(String(10))
+    pred= Column(String(50)) 
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/BTC")
+conn = engine.connect()
+
+#MODEL CONFIG#
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png','.jpeg']
 app.config['UPLOAD_PATH'] = 'static/uploads'
 model = load_model("model1.h5")
@@ -35,8 +56,27 @@ def upload_files():
         img_array = tf.expand_dims(img_array, 0)
         predictions = model.predict(img_array)
         score = tf.nn.softmax(predictions[0])
-    return render_template('index.html',prediction="This image most likely contains {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score)),filepath=upload_path)
+        prediction = class_names[np.argmax(score)]
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        age = request.form.get('age')
+        sex = request.form.get('sex')
+        patient = Patient(fname=fname,lname=lname,age=age,sex=sex,pred=prediction)
+        session = Session(bind=engine)
+        session.add(patient)
+        try:
+            session.commit()
+        except:
+            session.close()
+            abort(400)    
+        session.close()
+    return render_template('index.html',prediction="This image most likely contains {} with a {:.2f} percent confidence.".format(prediction, 100 * np.max(score)),filepath=upload_path)
 
-    
+@app.route('/patients')
+def patients():
+    session = Session(bind=engine)
+    rows = session.query(Patient)
+    return render_template('patients.html',rows = rows)    
+
 if __name__ == '__main__':
     app.run(debug=True)
